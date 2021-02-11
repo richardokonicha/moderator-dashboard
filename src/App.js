1d9be761271698ce0f7b1286a60d19ece34d7b3f
Richard Okonicha <richardokonicha@gmail.com>
2021-02-11 06:24:07 +0100

    initial dashboard setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./components/Dashboard/Dashboard";
+import { ThemeProvider } from "@material-ui/styles";
+import {  createMuiTheme } from "@material-ui/core";
+
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Dashboard/>
+    </ThemeProvider>
+
   );
 }
 
